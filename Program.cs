d424f020f74fe7a5b9d58a806325f8b81aab9067
Shustova <aleksa_shustova@mail.ru>
2026-09-21 09:59:51 +0300

    https://github.com/darashustova2305-stack/-Prac03_Shustova

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int number = 1_000_000;
+byte[] bytes = BitConverter.GetBytes(number);
+
+Console.WriteLine($"Число: {number}");
+Console.Write("Байты: ");
+foreach (byte b in bytes) {
+    Console.Write($"{b:X2} ");
+}
+Console.WriteLine();
